8938ad087b9fe54c4c122a952bfa8332f70b97d1
Jhulan Ganguly <118718124+jhulanganguly13@users.noreply.github.com>
2023-01-28 14:12:54 +0530

    Delete src directory

--- src/main/java/com/godisultimate/lms/repositories/DispatchDetailsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.godisultimate.lms.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.godisultimate.lms.model.DispatchDetails;
-
-public interface DispatchDetailsRepo extends JpaRepository<DispatchDetails, Long> {
-
-}
